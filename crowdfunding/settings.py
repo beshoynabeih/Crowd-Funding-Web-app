dcf4e8eb3408da5b48a19fd5527e0af415e1db15
Beshoy Nabeih <beshoybosha231@gmail.com>
2020-11-17 15:16:12 +0200

    updating settings

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project'
+    'project',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
