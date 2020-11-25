63d1616e1dca01495b4eaab2ab13140387b48998
Beshoy Nabeih <beshoybosha231@gmail.com>
2020-11-25 13:17:34 +0200

    updating settings file

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -123,22 +123,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
-<<<<<<< HEAD
-MEDIA_URL = 'media/'
-=======
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mohmed1997@gmai.com'
 EMAIL_HOST_PASSWORD = 'xyz123abc@'
-MEDIA_URL = '/media/'
->>>>>>> 5e97d5ea95bafd4743bcc091ce4d5f7970c0ecc6
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_URL = 'media/'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 LOGOUT_REDIRECT_URL = '/'
