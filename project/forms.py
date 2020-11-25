0dcbacb38ef37d1bdf79626294fcb9ed6d47dc91
Beshoy Nabeih <beshoybosha231@gmail.com>
2020-11-25 13:29:25 +0200

    integrate branches

--- project/forms.py
+++ project/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from project.models import Project, ProjectPicture
 from django.forms.widgets import DateInput
 from django.core.files.images import get_image_dimensions
-from project.models import UserProfile
+# from project.models import UserProfile
+
 
 class ProjectForm(forms.ModelForm):
     images = forms.ImageField(widget=forms.FileInput(attrs={'multiple': True}), required=False)
@@ -15,39 +16,3 @@ class ProjectForm(forms.ModelForm):
             'end_date': DateInput(attrs={'type': 'date'})
         }
 
-    class UserProfileForm(forms.ModelForm):
-      class Meta:
-        model = UserProfile
-
-      def clean_avatar(self):
-        avatar = self.cleaned_data['avatar']
-
-        try:
-            w, h = get_image_dimensions(avatar)
-
-            #validate dimensions
-            max_width = max_height = 100
-            if w > max_width or h > max_height:
-                raise forms.ValidationError(
-                    u'Please use an image that is '
-                     '%s x %s pixels or smaller.' % (max_width, max_height))
-
-            #validate content type
-            main, sub = avatar.content_type.split('/')
-            if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
-                raise forms.ValidationError(u'Please use a JPEG, '
-                    'GIF or PNG image.')
-
-            #validate file size
-            if len(avatar) > (20 * 1024):
-                raise forms.ValidationError(
-                    u'Avatar file size may not exceed 20k.')
-
-        except AttributeError:
-            """
-            Handles case when we are updating the user profile
-            and do not supply a new avatar
-            """
-            pass
-
-        return avatar
--- project/models.py
+++ project/models.py
@@ -88,7 +88,8 @@ class CommentReport(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
-      #create user profie
-class UserProfile(models.Model):
-    user  = models.OneToOneField(User)
-    avatar= models.ImageField()
+
+
+# class UserProfile(models.Model):
+#     user  = models.OneToOneField(User)
+#     avatar= models.ImageField()
